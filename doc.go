e84802398e96349fff66c93d02a727eda8bf5777
peterzandbergen <peter.zandbergen@myhops.com>
2015-07-27 22:27:55 +0200

    Added documentation to show on godoc.org.

--- doc.go
+++ doc.go
@@ -1,4 +1,7 @@
 /*
-gps2tcp document
+gps2tcp converts a gps modem connection to a telnet connection.
+It solves the problems of connecting with the Navigation program to a bluetooth
+serial port that is connected to an Android Phone running Bluetooth GPS Output
+app.
 */
 package gps2tcp
